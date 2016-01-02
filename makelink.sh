ceb9e475945ba7bde57f67615a3ee268c2d8f36b
uwap <me@uwap.name>
2016-01-02 23:26:01 +0100

    Add vim folder and create vim plugins as submodule

--- makelink.sh
+++ makelink.sh
@@ -1,6 +1,8 @@
 path=$(pwd)
 ln -fs $path/.feh.sh   ~/.feh.sh
-ln -fs $path/.vimrc    ~/.vimrc
+ln -fs $path/vim/vimrc ~/.vimrc
 ln -fs $path/.xinitrc  ~/.xinitrc
 ln -fs $path/.xmobarrc ~/.xmobarrc
 ln -fs $path/xmonad.hs ~/.xmonad/xmonad.hs
+
+ln -fs $path/vim/bundles/idris-vim ~/.vim/bundle/idris-vim
