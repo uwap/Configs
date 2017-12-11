465f848076edf2d09501781910e89d698a978d46
uwap <me@uwap.name>
2017-12-11 22:22:03 +0100

    update xmonad conf

--- makelink.sh
+++ makelink.sh
@@ -1,12 +1,14 @@
 path=$(pwd)
+
+mkdir -p ~/.xmonad
+mkdir -p ~/.vim/bundle/
+mkdir -p ~/.vim/autoload/
+
 ln -fs $path/.feh.sh   ~/.feh.sh
 ln -fs $path/vim/vimrc ~/.vimrc
 ln -fs $path/.xinitrc  ~/.xinitrc
 ln -fs $path/.xmobarrc ~/.xmobarrc
 ln -fs $path/xmonad.hs ~/.xmonad/xmonad.hs
 
-mkdir -p ~/.vim/bundle/
-mkdir -p ~/.vim/autoload/
-
 ln -fs $path/vim/bundles/* ~/.vim/bundle/
-ln -fs $path/vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+ln -fs $path/vim/autoload/pathogen.vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
--- xmonad.hs
+++ xmonad.hs
@@ -1,20 +1,29 @@
+{-# LANGUAGE DeriveDataTypeable, LambdaCase, MultiWayIf #-}
 import XMonad
 import XMonad.Hooks.DynamicLog
 import System.Exit
 import XMonad.StackSet hiding (workspaces)
 import Data.Map (Map, fromList)
-import Data.List (isInfixOf)
+import Data.List (isInfixOf, isSuffixOf)
 import Data.Char (toLower)
-import XMonad.Util.Run (safeSpawn)
+import XMonad.Util.Run
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
 import XMonad.Actions.CycleWS
 import XMonad.Layout.CenteredMaster
 import XMonad.Layout.Grid
 import XMonad.Layout.LayoutModifier (ModifiedLayout)
+import XMonad.Layout.WindowNavigation
+import XMonad.Layout.SubLayouts
+import XMonad.Layout.Spacing
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Tabbed
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Actions.MouseGestures
+import XMonad.Actions.Submap
+import XMonad.Actions.Navigation2D
 import XMonad.Util.EZConfig
 import Control.Monad
 import XMonad.Prompt
@@ -22,34 +31,58 @@ import XMonad.Prompt.Shell
 import XMonad.Prompt.Window
 import XMonad.Prompt.XMonad
 import XMonad.Prompt.Input
+import XMonad.Prompt.Pass
+import XMonad.Hooks.FadeInactive
+
+import XMonad.Layout.Decoration
+import XMonad.Util.Font
+import qualified XMonad.Util.Dzen as Dzen
+
+import qualified XMonad.Util.ExtensibleState as XS
+
+data KeyModes = Normal | Window deriving (Typeable, Show)
+instance ExtensionClass KeyModes where initialValue = Normal
 
 toggleBarKey :: XConfig Layout -> (KeyMask, KeySym)
 toggleBarKey XConfig {XMonad.modMask = modMask'} = (modMask', xK_b)
 
-xconf :: XConfig (Choose Tall (Choose (ModifiedLayout CenteredMaster Grid) Full))
-xconf = ewmh defaultConfig
+xconf = do
+    ewmh defaultConfig
       { modMask            = mod4Mask
-      , terminal           = "xfce4-terminal"
-      , borderWidth        = 1
-      , normalBorderColor  = "#000000"
-      , focusedBorderColor = "#000066"
-      , layoutHook         = Tall 1 (3/100) (1/2) ||| centerMaster Grid ||| Full
+      , terminal           = "xfce4-terminal --hide-menubar"
+      , borderWidth        = 2
+      , normalBorderColor  = "#2469a5"
+      , focusedBorderColor = "#e0116e"
+      , layoutHook         = myLayout
       , manageHook         = manageHook'
       , startupHook        = return () >> checkKeymap xconf keymap
       , keys               = keys'
       , workspaces         = workspaces'
       , handleEventHook    = fullscreenEventHook
       , mouseBindings      = mouseBindings'
+     -- , logHook            = fadeInactiveLogHook 0.8
       } where
-        workspaces' = ["main","web1"] ++ map (("dev" ++) . show) [1..6 :: Int] ++ ["game","chat","misc"]
+        workspaces' = ["misc","web1"] ++ map (("dev" ++) . show) [1..6 :: Int] ++ ["chat","game"]
         manageHook' = handleFullscreenFloat
           <+> manageHookShifts
           [ ("web1", ["Firefox"])
           , ("chat", ["Skype","Xchat","mail","Mail","Thunderbird"])
           , ("dev1", ["Eclipse"])
-          , ("misc", ["Clementine","clementine","Steam"])
+          , ("game", ["Steam"])
           ] <+> manageHookFloats []
         keys' conf = mkKeymap conf $ ("M-S-<Space>", setLayout $ layoutHook conf) : keymap
+
+myLayout = avoidStruts $ smartSpacingWithEdge 7
+    $ windowNavigation
+    $ Full ||| ThreeColMid 1 (3/100) (1/2) ||| ThreeCol 1 (3/100) (1/2)
+  where
+    titleTheme = def
+      { activeBorderColor = "#AAAAFF"
+      , inactiveBorderColor = "#7777FF"
+      , urgentBorderColor = "#FF7777"
+      , decoHeight = 18
+      , decoWidth = 100
+      }
 
 manageHookShifts :: [(String, [String])] -> ManageHook
 manageHookShifts = composeAll . concatMap (uncurry shiftToWS)
@@ -81,6 +114,14 @@ gestures = fromList [ ([L],   const nextWS)
 
 xprompt :: XPConfig
 xprompt = def { searchPredicate = isInfixOf
+              , bgColor = "#1B1D1E"
+              , fgColor = "#ACACAC"
+              , fgHLight = "#ebac54"
+              , bgHLight = "#1B1D1E"
+              , borderColor = "#1A1C1D"
+              , position = CenteredAt 0.5 1
+              , font = "-*-*-*-*-*-*-16-*-*-*-*-*-*-*"
+              , height = 24
               }
 
 keymap :: [(String, X ())]
@@ -88,18 +129,21 @@ keymap = join [programs, audio, layouts, focus', shrinkAndExpand, recompileAndQu
       where
         programs =
           [ ("M-<Return>", spawn $ terminal xconf)
-          , ("M-p"       , safeSpawn "dmenu_run" []) -- shellPrompt xprompt)
+          , ("M-p"       , shellPrompt xprompt)
           , ("M-S-p"     , windowPromptBring xprompt)
-          , ("M-o"       , windowPromptBring xprompt)
+          , ("M-y"       , passPrompt xprompt)
+          , ("M-S-y"     , passGeneratePrompt xprompt)
+       --   , ("M-o"       , windowPromptBring xprompt)
           , ("M-S-o"     , xmonadPrompt xprompt)
           , ("M-S-c"     , kill)
-          , ("M-w"       , safeSpawn "xtrlock" [])
+          , ("M-w"       , spawn "sleep .1; xtrlock-pam")
+          , ("M-,"       , spawn "sleep .1; xtrlock-pam")
           ]
         audio =
           [ ("<XF86AudioRaiseVolume>", safeSpawn "amixer" ["-q", "-D", "pulse", "sset", "Master", "1%+"])
           , ("<XF86AudioLowerVolume>", safeSpawn "amixer" ["-q", "-D", "pulse", "sset", "Master", "1%-"])
           , ("<XF86AudioMute>"       , safeSpawn "amixer" ["-q", "-D", "pulse", "sset", "Master", "toggle"])
-          , ("<XF86AudioPlay>"       , safeSpawn "mpc" ["toggle"])
+          , ("<XF86AudioPlay>"       , safeSpawn "vlc" ["--key-play-pause"])--safeSpawn "mpc" ["toggle"])
           , ("<XF86AudioNext>"       , safeSpawn "mpc" ["next"])
           , ("<XF86AudioPrev>"       , safeSpawn "mpc" ["prev"])
           ]
@@ -108,22 +152,43 @@ keymap = join [programs, audio, layouts, focus', shrinkAndExpand, recompileAndQu
       --  , ("M-S-<Space>", setLayout $ layoutHook conf)
           , ("M-t"        , withFocused $ windows . sink)
           , ("M-n"        , refresh)
+
+          , ("M-s"        , asks config >>= submap . defaultSublMap)
+          , ("M-i"        , XS.get >>= \case
+                    Normal -> sendMessage $ pushGroup U
+                    Window -> windowGo U True)
+          , ("M-j"        , XS.get >>= \case
+                    Normal -> sendMessage $ pushGroup L
+                    Window -> windowGo L True)
+          , ("M-k"        , XS.get >>= \case
+                    Normal -> sendMessage $ pushGroup D
+                    Window -> windowGo D True)
+          , ("M-l"        , XS.get >>= \case
+                    Normal -> sendMessage $ pushGroup R
+                    Window -> windowGo R True)
+          , ("M-S-v"      , withFocused (sendMessage . MergeAll))
+          , ("M-S-b"      , withFocused (sendMessage . UnMerge))
+          , ("M-b"        , sendMessage ToggleStruts)
           ]
         focus' =
           [ ("M-S-<Tab>"   , windows focusUp)
           , ("M-<Tab>"     , windows focusDown)
           , ("M-m"         , windows focusMaster)
           , ("M-S-<Return>", windows swapMaster)
-          , ("M-<Right>"   , nextWS)
+          , ("M-<Right>"   , XS.get >>= \x -> case x of
+                              Normal -> nextWS
+                              Window -> windows swapMaster)
           , ("M-<Left>"    , prevWS)
-          , ("M-j"         , prevScreen)
-          , ("M-k"         , nextScreen)
+          , ("M-u"         , prevScreen)
+          , ("M-o"         , nextScreen)
+          , ("M-e"         , XS.put Normal)
+          , ("M-S-e"       , XS.put Window)
           ]
         shrinkAndExpand =
-          [ ("M-,", sendMessage (IncMasterN 1))
-          , ("M-.", sendMessage (IncMasterN (-1)))
-          , ("M-h", sendMessage Shrink)
-          , ("M-l", sendMessage Expand)
+      --    [ ("M-,", sendMessage (IncMasterN 1))
+      -- , ("M-.", sendMessage (IncMasterN (-1)))
+          [ ("M-h", sendMessage Shrink)
+          , ("M-z", sendMessage Expand)
           ]
         recompileAndQuit =
           [ ("M-S-q", io exitSuccess)
@@ -136,7 +201,7 @@ keymap = join [programs, audio, layouts, focus', shrinkAndExpand, recompileAndQu
           , ("M-S-k"      , shiftNextScreen)
           ]
         keyboardLayouts =
-          [ ("M-<F1>"  , spawn "setxkbmap -layout dvp")
+          [ ("M-<F1>"  , spawn "setxkbmap -layout us; setxkbmap -variant dvp")
           , ("M-S-<F1>", spawn "setxkbmap -layout de")
           ]
         screensaver =
@@ -145,19 +210,38 @@ keymap = join [programs, audio, layouts, focus', shrinkAndExpand, recompileAndQu
           ]
         ynInput str action = inputPrompt xprompt str ?+ \x -> when ("y" `isInfixOf` map toLower x || "j" `isInfixOf` map toLower x) action
 
-xmobarpp :: PP
-xmobarpp = xmobarPP
-  { ppSep = " "
-  , ppOrder = \(ws:_:t:_) -> [ws,t]
-  , ppTitle = formatTitle
+dzenpp :: PP
+dzenpp = dzenPP
+  { ppSep = ""
+  , ppOrder = \p -> case p of
+      ws:s:xs -> s:ws:xs
+      xs -> xs
+  , ppLayout = formatLayout
+  , ppCurrent         = dzenColor "#ebac54" "" . (++ " ")
+  , ppVisible         = dzenColor "#ACACAC" "" . (++ " ")
+  , ppHidden          = dzenColor "white" "" . (++ " ")
+  , ppHiddenNoWindows = dzenColor "#7b7b7b" "" . (++ " ")
+  , ppUrgent          = dzenColor "#eb7254" "" . (++ " ")
+  , ppTitle           = (" " ++) . dzenColor "#ACACAC" "" . dzenEscape
+ -- , ppOrder = \(ws:a:t:m:xs) -> ws:a:t:m:xs
+--  , ppTitle = formatTitle
+--  , ppHidden = xmobarColor "white" "grey"
+--  , ppCurrent = xmobarColor "white" "black"
+ -- , ppExtras = [Just . show <$> (XS.get :: X KeyModes)] 
   } where
-    formatTitle :: String -> String
-    formatTitle str = let wntitle = shorten 40 str in
-      replicate (40 - length wntitle) ' '
-      ++ xmobarColor "green" "black" (wrap "<<" ">>" $ xmobarColor "grey" "black" wntitle)
+      formatLayout x = dzenColor "#ebac54" "#1B1D1E" $
+        " ^ca(1, xdotool keydown super key space keyup super)" ++ layoutImage x ++ "^ca() "
+      layoutImage x = case x of
+        _ | "ThreeCol" `isSuffixOf` x -> "^i(/home/uwap/.xmonad/dzen2/three.xbm)"
+        _ | "Full" `isSuffixOf` x -> "^i(/home/uwap/.xmonad/dzen2/full.xbm)"
+        _ | otherwise -> x
 
 main :: IO ()
 main = do
   let uhook = withUrgencyHookC NoUrgencyHook UrgencyConfig { suppressWhen = Focused, remindWhen = Dont }
-  statBar <- statusBar "xmobar" xmobarpp toggleBarKey $ uhook xconf
+  let dzenLeft = "dzen2 -dock -x '0' -y '0' -w '1000' -ta 'l' -h 24 -fn '-*-sans-*-*-*-*-*-*-*-*-*-*-*-*'"
+  let dzenRight = "conky -c /home/uwap/.xmonad/.conky_dzen | dzen2 -dock -x '1000' -w '920' -ta 'r' -y '0' -h 24 -fn '-*-sans-*-*-*-*-*-*-*-*-*-*-*-*'"
+  spawnPipe dzenRight
+  spawn "while true; do feh --bg-scale ~/Bilder/wallpaper/ --randomize; sleep 3000; done"
+  statBar <- statusBar dzenLeft dzenpp toggleBarKey $ uhook xconf
   xmonad statBar
